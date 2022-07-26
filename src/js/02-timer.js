b4bb7cf1da3ac9c6174b7377a174a60bc5bfe355
Viacheslav Maksymyshen <slavamax.it@gmail.com>
2022-07-26 21:51:51 +0300

    Task - 2 added flatpickr

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,16 +3,32 @@ import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
 
-const datetimePicker = document.querySelector('#datetime-picker');
-const dataStart = flatpickr(datetimePicker, {});
-console.log('🚀 ~ file: 02-timer.js ~ line 8 ~ dataStart', dataStart);
+const btnStart = document.querySelector('[data-start]');
 
+let dataStartObj = 0;
+
+let currentTime = Date.now();
+
+btnStart.disabled = true;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    dataStartObj = selectedDates[0];
+    if (dataStartObj > currentTime) {
+      btnStart.disabled = false;
+    }
+    console.log(dataStartObj);
   },
 };
+
+flatpickr('#datetime-picker', options);
+function timerStart() {
+  console.log('go');
+}
+btnStart.addEventListener('click', timerStart);
+
+console.log('🚀 ~ file: 02-timer.js ~ line 9 ~ dataStartObj', dataStartObj);
+console.log('🚀 ~ file: 02-timer.js ~ line 10 ~ currentTime', currentTime);
